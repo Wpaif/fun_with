390e327915cd077a3b83ceacace3910253f17be0
Wilian Ferreira <wpaife@gmail.com>
2022-10-10 16:07:13 -0300

    Least common multiple

---
+++ C Lang/5. Comandos de repetição/lcm.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+  int lcm, aux, i, num1, num2;
+  printf("Type two numbers:\n");
+  scanf("%d %d", &num1, &num2);
+  setbuf(stdin, NULL);
+
+  do
+  {
+    if (num1 > num2)
+    {
+      aux = num1;
+      num1 = num2;
+      num2 = aux;
+    }
+
+    for (i = 2; i <= num2; i++)
+    {
+      aux = i * num1;
+      if (!(aux % num2))
+      {
+        lcm = aux;
+        i = num2 + 1;
+      }
+    }
+    system("clear");
+
+    printf("LMC(%d, %d): %d\n", lcm, num1, num2);
+
+    printf("\nType two numbers:\n");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+  } while (num1 && num2);
+}
--- C Lang/5. Comandos de repetição/primes_numbers_in_range.c
+++ C Lang/5. Comandos de repetição/primes_numbers_in_range.c
@@ -6,22 +6,26 @@ int main(void)
   int upper_limit, lower_limit, aux, count;
   printf("Type the lower limit and the upper limit:\n");
   scanf("%d %d", &lower_limit, &upper_limit);
-  
-  if (lower_limit > upper_limit){
+
+  if (lower_limit > upper_limit)
+  {
     int aux = lower_limit;
     lower_limit = upper_limit;
     upper_limit = aux;
   }
-  
+
   system("clear");
   printf("Range: [%d, %d]\n", lower_limit, upper_limit);
   printf("Primes numbers: ");
 
-  while (lower_limit <= upper_limit){
+  while (lower_limit <= upper_limit)
+  {
     aux = lower_limit++;
     count = 0;
-    for (size_t i = 1; i <= aux; i++){
-      if (aux == 1){
+    for (size_t i = 1; i <= aux; i++)
+    {
+      if (aux == 1)
+      {
         printf("%d ", aux);
         break;
       }
