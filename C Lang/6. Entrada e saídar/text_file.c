14607f41038498cb319d1fe80382d35d48f4c529
Wilian Ferreira <wpaife@gmail.com>
2022-10-18 00:38:29 -0300

    Reading and writing in text file

--- C Lang/6. Entrada e saídar/text_file.c
+++ C Lang/6. Entrada e saídar/text_file.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
-int main() {
-  int a, b, x;
-  FILE *f1;
-  f1 = fopen("infile.txt", "r");
-  x = fscanf(f1, "%d %d", &a, &b);
-  printf("a: %2d\nb: %2d\n\n", a, b);
+#include <stdlib.h>
 
-  while (x > 0) {
+int main() {
+  int a, b, x, sum = 0;
+  FILE *f1, *f2;
+
+  f1 = fopen("infile.txt", "w");
+  for (int i = 1; i <= 22; i++)
+    if (i != 22)
+      fprintf(f1, "%d ", 7 * i);
+    else
+      fprintf(f1, "%d", 7 * i);
+
+  fclose(f1);
+  system("bat infile.txt");
+
+  f1 = fopen("infile.txt", "r");
+  f2 = fopen("outline.txt", "w");
+
+  do {
     x = fscanf(f1, "%d %d", &a, &b);
-    printf("Readers: %d\n", x);
-    printf("a: %2d\nb: %2d\n\n", a, b);
-  }
+    if (x == 2)
+      sum += a + b;
+    else
+      sum += a;
+  } while (x == 2);
+
+  fprintf(f2, "Sum %d", sum);
+  fclose(f2);
+  f2 = fopen("outline.txt", "r");
+  fclose(f2);
+  system("bat outline.txt");
 }
