4a5eea9d522dd65de4cd94b717106aa66eedc41a
Wilian Ferreira <wpaife@gmail.com>
2023-01-23 16:56:13 -0300

    Organizing directories

---
+++ C/5. Comandos de repetição/mclaurim_serie_trygonometry.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include <stdio.h>
+
+#define PI 3.142857
+#define loops 10 
+
+long int factorial(short int n);
+
+int main() {
+  int angle;
+  long double sen = 0.0, cos = 0.0, aux;
+  do {
+    printf("Type an angle: ");
+    scanf("%d", &angle);
+
+    aux = PI * angle / 180;
+
+    for (size_t i = 0; i <= loops; i++) {
+      sen += pow(-1, i) * (pow(aux, 2 * i + 1) / factorial(2 * i + 1));
+      cos += pow(-1, i) * (pow(aux, 2 * i) / factorial(2 * i));
+    }
+
+    // Disregards the study of the signal by the Cartesian plane
+    printf("sin(%d): %.16Lf\n", angle, sen);
+    printf("cos(%d): %.16Lf\n\n", angle, cos);
+
+  } while (angle > 0);
+
+  return 0;
+}
+
+long int factorial(short n) {
+  if (n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
+}
